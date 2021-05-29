daa2d3560f6ab43318ad8e87945df2d71a6a2951
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-29 18:46:50 -0500

    added delay pipe and refresh for equation and answer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'mathform'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mathform');
   });
 
   it('should render title', () => {
--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { delay } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -28,6 +29,19 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.mathForm.statusChanges.pipe(
+      delay(100)
+    ).subscribe((value) => {
+      if (value === 'INVALID') {
+        return;
+      }
+
+      this.mathForm.setValue({
+        a: this.randomNumber(),
+        b: this.randomNumber(),
+        answer: ''
+      })
+    })
   }
 
   randomNumber() {
